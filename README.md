8e3a396a21d45e3d80033d3176ca37477059405e
Dave Morz <35872932+HootOwl17@users.noreply.github.com>
2018-04-07 10:11:13 -0600

    Agregando descripciones mas detalladas del codigo

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Join-07Apr2018
 
 Repositorio para códigos de practica del 07/April/2018
+
+Explicacion de cada Archivo
+
+#Index.js
+Archivo que se abre por default, en esta la Informacion se crea y se muestra luego de 2 formas distintas.
+Y guarda la Informacion en otro archivo llamado "DATA.JSON".
+
+#DATA.JSON
+Archivo que guarda la Informacion que manda la Informacion...
+(Seria mejor una Base de Datos, pero es lo unico que tenemos por ahora :'v)
+
+#guardado.json
+Archivo de lectura hecho accidentalmente para guardar la Informacion, pero tambien fue buena para ver si funcionaba bien el codigo.
+
 # [Pakech :v](https://goo.gl/oGWgwq)
--- index.js
+++ index.js
@@ -7,13 +7,13 @@ console.log("El nombre es "+Informacion["nombre"]);// 2da forma para presentar I
 
 var fs= require('fs');
 
-var Archivo = fs.readFileSync("data.JSON");
-var DataArchivo = JSON.parse(Archivo);
+var Archivo = fs.readFileSync("data.JSON");// lee lo que el archivo data.json tiene
+var DataArchivo = JSON.parse(Archivo);// Escribe la nueva Informacion para la variable Archivo
 
 console.log("El correo es "+ DataArchivo.correo);
 
 DataArchivo.edad = 19;//1ra forma para agregar info a la Base de Datos
-DataArchivo["Sexo"] = "Aveces";
+DataArchivo["Sexo"] = "Aveces";//2da Forma para agregar info a la Base de Datos
 
 var GuardarArchivo = JSON.stringify(DataArchivo);
-fs.writeFileSync('DATA.json',GuardarArchivo);
+fs.writeFileSync('DATA.json',GuardarArchivo);//Guarda la info en DATA.Json
