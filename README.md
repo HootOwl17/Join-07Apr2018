9aaa25c3a6a6fa29c680d9b577432b384f2ac7a3
Dave Morz <35872932+HootOwl17@users.noreply.github.com>
2018-04-07 09:19:07 -0600

    Primer Ejercicio

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Join-07Apr2018
+
+Repositorio para códigos de practica del 07/April/2018
+# [Pakech :v](https://goo.gl/oGWgwq)
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Iniciando");
+
+var Informacion = {"nombre":"Pepe"}
+console.log("El nombre es "+ Informacion.nombre);
+console.log("El nombre es "+Informacion["nombre"]);
