5833ee44eeefdd0d46f3ce4083d087bdd0fa7e1c
Dave Morz <35872932+HootOwl17@users.noreply.github.com>
2018-04-07 09:38:21 -0600

    Creando una Base de Datos y Agrengando mas descripcion

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 console.log("Iniciando");
 
 var Informacion = {"nombre":"Pepe"}
-console.log("El nombre es "+ Informacion.nombre);
-console.log("El nombre es "+Informacion["nombre"]);
+//var DataInfo = JSON.parse(Informacion);
+console.log("El nombre es "+ Informacion.nombre);//1ra forma para presentar Informacion
+console.log("El nombre es "+Informacion["nombre"]);// 2da forma para presentar Informacion
+
+var fs= require('fs');
+
+var Archivo = fs.readFileSync("data.JSON");
+var DataArchivo = JSON.parse(Archivo);
+
+console.log("El correo es "+ DataArchivo.correo);
+
+DataArchivo.edad = 19;// para agregar info a la Base de Datos
